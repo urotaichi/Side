a2206942ebf9c81718fcf0657a56cc5fd79e293e
urotaichi <admin@urotaichi.com>
2021-10-10 12:58:12 +0900

    Revert "CanvasMasao.jsを最新のもの(v5.2.10)に変更"
    This reverts commit ab91d48f858867cedede3671abf4a16a6fda5a60.

--- side/Definition.cs
+++ side/Definition.cs
@@ -68,10 +68,10 @@ namespace MasaoPlus
 		public string Version = FileVersionInfo.GetVersionInfo((new Uri(Assembly.GetExecutingAssembly().CodeBase)).LocalPath).FileVersion;
 
 		// Token: 0x04000272 RID: 626
-		public double CheckVersion = 2.72;
+		public double CheckVersion = 2.71;
 
 		// Token: 0x04000273 RID: 627
-		public string EditorIdStr = "/* [MI]Created By:Side - the Supermasao Integrated Development Environment v2.7.2[/MI] */";
+		public string EditorIdStr = "/* [MI]Created By:Side - the Supermasao Integrated Development Environment v2.7.1[/MI] */";
 
 		// Token: 0x04000274 RID: 628
 		public bool IsAutoUpdateEnabled = true;
