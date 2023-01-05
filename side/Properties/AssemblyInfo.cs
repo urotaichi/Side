3dcb710a0429303ad3009e784b5ef2c88be789a1
urotaichi <admin@urotaichi.com>
2023-01-05 22:11:27 +0900

    2023

--- side/Properties/AssemblyInfo.cs
+++ side/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Security.Permissions;
 [assembly: Guid("a6fc8342-d888-44eb-b337-d4879f7150f8")]
 [assembly: AssemblyProduct("Supermasao Integrated Development Environment")]
 [assembly: AssemblyDescription("正男エディタ")]
-[assembly: AssemblyCopyright("© 2022 urotaichi")]
+[assembly: AssemblyCopyright("© 2023 urotaichi")]
 [assembly: AssemblyCompany("urotaichi corporation")]
 [assembly: AssemblyConfiguration("")]
 //[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
