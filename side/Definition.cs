eb20a6585406139b7f84908e909d0a93f6a3c258
urotaichi <admin@urotaichi.com>
2023-07-09 14:52:50 +0900

    バージョンアップ

--- side/Definition.cs
+++ side/Definition.cs
@@ -46,9 +46,9 @@ namespace MasaoPlus
 
 		public string Version = FileVersionInfo.GetVersionInfo((new Uri(Assembly.GetExecutingAssembly().CodeBase)).LocalPath).FileVersion;
 
-		public double CheckVersion = 2.91;
+		public double CheckVersion = 2.911;
 
-		public string EditorIdStr = "/* [MI]Created By:Side - the Supermasao Integrated Development Environment v2.10.0[/MI] */";
+		public string EditorIdStr = "/* [MI]Created By:Side - the Supermasao Integrated Development Environment v2.11.0[/MI] */";
 
 		public bool IsAutoUpdateEnabled = true;
 
