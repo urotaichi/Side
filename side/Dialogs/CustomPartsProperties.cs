cfed283f32f0c7be6bb54a066042a61160ca688c
urotaichi <admin@urotaichi.com>
2023-08-05 16:50:44 +0900

    カスタムパーツ用　パラメータ定義

---
+++ side/Dialogs/CustomPartsProperties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasaoPlus.Dialogs
+{
+    public struct CustomPartsProperties
+    {
+        public int walk_speed;
+
+        public int fall_speed;
+
+        public int jump_vy;
+
+        public int search_range;
+
+        public int interval;
+
+        public int period;
+
+        public Dictionary<int, int> attack_timing;
+
+        public int speed;
+
+        public int accel;
+
+        public int distance;
+
+        public int attack_speed;
+
+        public int return_speed;
+
+        public int speed_x;
+
+        public int speed_y;
+
+        public int radius;
+
+        public int init_vy;
+    }
+}
