07d69bbd4e42acbda69bd752fea1e1a660e47974
urotaichi <admin@urotaichi.com>
2023-09-04 21:28:08 +0900

    StageSizeDataのコピー関数実装

--- side/Runtime.cs
+++ side/Runtime.cs
@@ -153,6 +153,11 @@ namespace MasaoPlus
 
                 [XmlAttribute]
                 public int bytesize;
+
+                public StageSizeData Clone()
+                {
+                    return (StageSizeData)MemberwiseClone();
+                }
             }
         }
 
