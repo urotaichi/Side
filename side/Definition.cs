8a225f226c6070a188cddeee00d721fb3121ab29
GitHub Action <action@github.com>
2025-07-05 06:33:30 +0000

    Update version to 4.7.0

--- side/Definition.cs
+++ side/Definition.cs
@@ -55,7 +55,7 @@ namespace MasaoPlus
 
         public string Version;
 
-        public double CheckVersion = 4.60;
+        public double CheckVersion = 4.70;
 
         public string EditorIdStr;
 
